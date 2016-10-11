ba87104a9da716a24c0a02ee78eceb1fc908d960
nkirov <nkkirov@gmail.com>
2016-10-11 15:50:34 +0300

    so

---
+++ 2017/CITB100/sources/ch7/words.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{  int count = 0;
+   string word;
+   while (cin >> word) count++;
+
+   cout << count << " words.\n";
+   return 0;
+}
