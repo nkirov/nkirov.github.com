64bdd045a06f8cd285e56bc41d85950b1e6dd01f
Nikolay Kirov <nkirov@Kirils-MBP.lan>
2018-06-06 09:23:35 +0300

    ps

---
+++ 2018/CITB201/sources/exept.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <stdexcept>
+using namespace std;
+
+void f()
+{  cout << "f" << endl;
+    throw runtime_error("RTE");
+}
+    
+void ff() throw ()
+{
+    cout << "ff1" << endl;
+    f();
+    cout << "ff2" << endl;
+}
+
+int main()
+{  try
+    {  ff();
+        cout << "main" << endl;
+    }
+    catch(runtime_error& r)  // runtime_error& r)
+    { cout << "2. " << r.what() << endl;
+    }
+    
+    return 0;
+    }
