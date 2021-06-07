fa362f3ea7cf0231a57145a5bb0f587df8dd58df
nkirov <nkkirov@gmail.com>
2021-06-07 09:00:54 +0300

    ae

--- 2021/CITB205/sources/exept.cpp
+++ 2021/CITB205/sources/exept.cpp
@@ -3,24 +3,28 @@
 using namespace std;
 
 void f()
-{  cout << "f" << endl;
+{
+    cout << "f" << endl;
     throw 22;
-   throw runtime_error("RTE");
+    throw runtime_error("RTE");
 }
     
 void ff()
-{   cout << "ff1" << endl;
+{
+    cout << "ff1" << endl;
     f();
     cout << "ff2" << endl;
 }
 
 int main()
 {   try
-    {  ff();
+    {
+        ff();
         cout << "main" << endl;
     }
     catch(int k)  // runtime_error& r)
-    { cout << "2. " << k << endl;
+    {
+        cout << "2. " << k << endl;
     }
     return 0;
 }
