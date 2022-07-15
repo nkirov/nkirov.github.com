be000f229be5090dbf889c6a091417dd124cd386
Nikolay Kirov <nkkirov@gmail.com>
2022-07-15 15:13:12 +0300

    new

---
+++ 2023/CITB107/sources/ch4/area1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{ cout << 36412%25 ;
+     double area;
+   cout << "Please enter the area of a square: ";
+   cin >> area;
+   if (area < 0)
+   {  cout << "Error: Negative area.\n";
+      return 1;
+   }
+
+   /* now we know that area is >= 0  */
+   double length = sqrt(area);
+   cout << "The side length of the square is " 
+        << length << "\n";
+
+   return 0;
+}
