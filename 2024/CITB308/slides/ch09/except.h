ab517219fa2b8fab65780fb4b09add99f4186e89
Nikolay Kirov <nkkirov@gmail.com>
2023-07-27 09:03:39 +0300

    new

---
+++ 2024/CITB308/slides/ch09/except.h
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class RuntimeException {		// generic run-time exception
+private:
+  string errorMsg;
+public:
+  RuntimeException(const string& err) { errorMsg = err; }
+  string getMessage() const { return errorMsg; }
+};
+/**
+ * Exception thrown on performing top or pop of an empty stack.
+ */
+class NonexistentElementException : public RuntimeException {  
+public:
+  NonexistentElementException(const string& err) : RuntimeException(err) {}
+};
+
